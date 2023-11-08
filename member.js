12be230fcf9e1a9b896210b67075a024b2aace1e
Elvis Gomez Floegel <37124582+eogomezf@users.noreply.github.com>
2023-11-08 02:38:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,27 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'PHP', 'MySQL'];
+    var member = {
+        name: 'John',
+        age: 25,
+        skills: skills,
+        // skills: ['HTML', 'CSS', 'JS', 'PHP', 'MySQL'],
+        // skills: function () {
+        //     return ['HTML', 'CSS', 'JS', 'PHP', 'MySQL'];
+        // },
+        // skills: () => ['HTML', 'CSS', 'JS', 'PHP', 'MySQL'],
+        // skills: () => {
+        //     return ['HTML', 'CSS', 'JS', 'PHP', 'MySQL'];
+        // },
+        // skills: () => {
+        //     return skills;
+        // },
+        // skills: () => {
+        //     return this.skills;
+        // },
+        // skills: function () {
+        //     return this.skills;
+        // },
+        // skills: function () {
+        //     return skills;
+        // },
+        // skills: function () {
